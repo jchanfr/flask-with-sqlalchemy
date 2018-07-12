5a6c60c78cb934adf803f7beb416a84c217c8b79
jchanfr <40856570+jchanfr@users.noreply.github.com>
2018-07-12 15:21:59 +0200

    Update xx.bat

--- xx.bat
+++ xx.bat
@@ -1,6 +1,6 @@
 curl -X POST -d '{ "name": "nom" }'  -H 'Content-Type: application/json' http://localhost:5000/api/v1/products
 
-curl -X PATCH -d '{ "name": "AFFREUX" }'  -H 'Content-Type: application/json' http://localhost:5000/api/v1/products
-/2
-curl -X DELETE    http://localhost:5000/api/v1/products
-/3
+curl -X PATCH -d '{ "name": "AFFREUX" }'  -H 'Content-Type: application/json' http://localhost:5000/api/v1/products/2
+
+curl -X DELETE    http://localhost:5000/api/v1/products/3
+
