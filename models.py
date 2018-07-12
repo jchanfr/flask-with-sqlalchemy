753fb1874c835ae4ffd3a9073e268844ef0e9330
jacques <jchanfr@gmail.com>
2018-07-12 15:14:30 +0200

    j4

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class Product(db.Model):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    description = db.Column(db.Text())
+
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
--- wsgi.py
+++ wsgi.py
@@ -23,13 +23,47 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 from models import Product
-from schemas import products_schema
+from schemas import products_schema,product_schema
+from flask import request
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
-@app.route('/api/v1/products')
-def products():
-    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
-    return products_schema.jsonify(products)
+@app.route('/api/v1/products',methods=['GET','POST'])
+def use_products():
+    if (request.method=='GET'):
+        products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+        return products_schema.jsonify(products)
+    elif (request.method=='POST'):
+        products = db.session.query(Product).all()
+        data=request.json
+        product=Product(name=data["name"])
+        db.session.add(product)
+        db.session.commit()
+        return product_schema.jsonify(product)
+
+
+@app.route('/api/v1/products/<id>',methods=['GET','DELETE','PATCH'])
+def manage_products(id):
+    if (request.method=='GET'):
+        product=db.session.query(Product).filter_by(id=int(id)).first()
+        return product_schema.jsonify(product)
+    elif (request.method=='DELETE'):
+        product=db.session.query(Product).filter_by(id=int(id)).first()
+        db.session.delete(product)
+        db.session.commit()
+        products = db.session.query(Product).all()
+        return products_schema.jsonify(products)
+
+    elif (request.method=='PATCH'):
+        data=request.json
+        product=db.session.query(Product).filter_by(id=int(id)).first()
+        product.name=data["name"]
+        db.session.commit()
+        products = db.session.query(Product).all()
+        return products_schema.jsonify(products)
+
+
+
+
