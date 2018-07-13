2a2a2e228c29a7652faf8ad74ea172adc5781421
jacques <jchanfr@gmail.com>
2018-07-13 14:19:18 +0200

    j5

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,9 @@ except:
 
 from flask import Flask
 from config import Config
-
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -24,37 +26,40 @@ ma = Marshmallow(app)
 
 from models import Product
 from schemas import products_schema,product_schema
-from flask import request
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('login.html')
 
 @app.route('/api/v1/products',methods=['GET','POST'])
 def use_products():
     if (request.method=='GET'):
         products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
-        return products_schema.jsonify(products)
+        return render_template('bootstrap.html',products=products)
     elif (request.method=='POST'):
         products = db.session.query(Product).all()
         data=request.json
         product=Product(name=data["name"])
         db.session.add(product)
         db.session.commit()
-        return product_schema.jsonify(product)
+        render_template('prod.html',products=products)
+
 
 
 @app.route('/api/v1/products/<id>',methods=['GET','DELETE','PATCH'])
 def manage_products(id):
     if (request.method=='GET'):
         product=db.session.query(Product).filter_by(id=int(id)).first()
-        return product_schema.jsonify(product)
+#        return product_schema.jsonify(product)
+        render_template('prod.html',products=product)
+
     elif (request.method=='DELETE'):
         product=db.session.query(Product).filter_by(id=int(id)).first()
         db.session.delete(product)
         db.session.commit()
         products = db.session.query(Product).all()
-        return products_schema.jsonify(products)
+        render_template('prod.html',products=products)
+#        return products_schema.jsonify(products)
 
     elif (request.method=='PATCH'):
         data=request.json
@@ -62,8 +67,5 @@ def manage_products(id):
         product.name=data["name"]
         db.session.commit()
         products = db.session.query(Product).all()
-        return products_schema.jsonify(products)
-
-
-
-
+#        return products_schema.jsonify(products)
+        render_template('prod.html',products=products)
